15ae61e504517a36254dec94abca81fbd92f6414
Ceciliahelberg <272983@uis.no>
2026-09-11 12:03:25 +0200

    Oppgave 6: plotter døgnprofilen, lagret plottet som png

--- Oving4.py
+++ Oving4.py
@@ -17,3 +17,19 @@ print(df.head())
 
 # Oppgave 5
 print(df.index[0])
+
+# Oppgave 6
+print(df.loc["2026-01-01 03:00"])
+
+# Henter ut last data for et døgn (9.mars)
+døgn = df.loc["2026-03-09 00:00":"2026-03-09 23:00", ["Consumption"]]
+print(døgn)
+
+plot = døgn.plot(figsize=(10, 6), color="hotpink")
+plt.title("Consumption on March 9, 2026")
+plt.xlabel("Time")
+plt.ylabel("Consumption")
+plt.grid(True)
+plt.savefig(r"C:\repos\ELK330-Oving4\oppgave6.png")
+plt.show()
+
